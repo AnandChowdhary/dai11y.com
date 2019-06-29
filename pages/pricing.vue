1deaf157fef97fe61965f3c741201bb15347b527
Anand Chowdhary <mail@anandchowdhary.com>
2019-06-29 11:48:59 +0200

    :sparkles: Add pricing

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,5 +1,357 @@
 <template>
-  <main itemscope itemtype="https://schema.org/Product"></main>
+  <main itemscope itemtype="https://schema.org/Product">
+    <div class="hero hero--align-center">
+      <h1>Simple, transparent pricing.</h1>
+      <p>
+        Track your first 3 webpages for free and switch to unlimited auditing
+        with extra features and hourly audits you're ready.
+      </p>
+    </div>
+    <div
+      class="container"
+      itemprop="offers"
+      itemscope
+      itemtype="https://schema.org/Offer"
+    >
+      <div class="row row--padding-large hide-mobile">
+        <div></div>
+        <div>
+          <div class="card card--type-padded text text--align-center">
+            <h2 class="plan">Free</h2>
+            <div class="price">
+              <span itemprop="priceCurrency" content="USD">$</span
+              ><span itemprop="price" content="0.00">0</span>
+              <link itemprop="availability" href="https://schema.org/InStock" />
+            </div>
+            <div class="text text--size-small text--color-light">&nbsp;</div>
+            <div class="text text--size-small text--color-light">&nbsp;</div>
+          </div>
+        </div>
+        <div>
+          <div class="card card--type-padded text text--align-center">
+            <h2 class="plan">Pro</h2>
+            <div class="price">
+              <span itemprop="priceCurrency" content="USD">$</span
+              ><span itemprop="price" content="49.00">49</span>
+              <link itemprop="availability" href="https://schema.org/InStock" />
+            </div>
+            <div class="text text--size-small text--color-light">per month</div>
+            <div class="text text--size-small text--color-light">
+              billed monthly
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="card card--type-padded text text--align-center">
+            <h2 class="plan">Enterprise</h2>
+            <div class="price">
+              <span>Custom</span>
+              <link itemprop="availability" href="https://schema.org/InStock" />
+            </div>
+            <div class="text text--size-small text--color-light">
+              unlimited users
+            </div>
+            <div class="text text--size-small text--color-light">
+              billed annually
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row--padding-large">
+        <div class="hide-mobile">
+          <div
+            class="card card--type-padded card--type-invisible"
+            style="padding-left: 0"
+          >
+            <ul>
+              <li>Webpages</li>
+              <li>Status pages</li>
+              <li>Audits</li>
+              <li>Daily metrics</li>
+              <li>Alert notifications</li>
+              <li>Hourly audits</li>
+              <li>Support</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div class="card card--type-padded text text--align-center">
+            <ul>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>3 monitors</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>1 public page</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>All 5 audits</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>1 month history</span>
+              </li>
+
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Email, Slack</span>
+              </li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+            </ul>
+            <div class="text text--mt-2">
+              <router-link
+                to="/auth/register?plan=free"
+                class="button button--size-large"
+                >Get started</router-link
+              >
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="card card--type-padded text text--align-center">
+            <ul>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Unlimited</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Unlimited</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>All 5 audits</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Unlimited history</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Email, Slack, SMS, more</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Hourly, daily, weekly</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Chat support</span>
+              </li>
+            </ul>
+            <div class="text text--mt-2">
+              <router-link
+                to="/auth/register?plan=pro"
+                class="button button--color-primary button--size-large"
+              >
+                <span>Get started</span>
+                <font-awesome-icon icon="arrow-right" class="icon icon--ml-1" />
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="card card--type-padded text text--align-center">
+            <ul>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Unlimited</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Unlimited</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>All 5 audits</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Unlimited history</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Email, Slack, SMS, more</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Hourly, daily, weekly</span>
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+                <span>Dedicated support</span>
+              </li>
+            </ul>
+            <div class="text text--mt-2">
+              <router-link
+                to="/auth/register?plan=contact"
+                class="button button--size-large"
+                >Contact us</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row--padding-large text text--mt-4 hide-mobile">
+        <div>
+          <ul>
+            <li>100+ integrations</li>
+            <li>Team members</li>
+            <li>Developer API</li>
+            <li>Bank-grade security</li>
+            <li>Hosted in Europe</li>
+            <li>Data export &amp; delete</li>
+            <li>99.99% uptime SLA</li>
+            <li>Increased rate limit</li>
+            <li>Single-sign on</li>
+            <li>IP whitelisting</li>
+            <li>Self-hosted service</li>
+            <li>Reseller accounts</li>
+          </ul>
+        </div>
+        <div>
+          <div class="text text--align-center">
+            <ul>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div class="text text--align-center">
+            <ul>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+              <li><font-awesome-icon class="i-n" icon="times" title="No" /></li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div class="text text--align-center">
+            <ul>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+              <li>
+                <font-awesome-icon class="i-y" icon="check" title="Yes" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <section class="section">
+        <div class="text text--align-center">
+          <h2>More plans</h2>
+        </div>
+        <div class="addons more-plans">
+          <div class="card card--type-padded">
+            <h3>For open-source</h3>
+            <p>
+              Pro plan availabile for free for open-source projects under
+              permissive licenses.
+            </p>
+            <a href="#">Apply for FOSS &rarr;</a>
+          </div>
+          <div class="card card--type-padded">
+            <h3>For students</h3>
+            <p>
+              Free or discounted Startup plan for student developers with an
+              educational account
+            </p>
+            <a href="#">Create student account &rarr;</a>
+          </div>
+          <div class="card card--type-padded">
+            <h3>For non-profits</h3>
+            <p>
+              Up to 50% off on all plans and services for registered text
+              501(c)(3) non-profits
+            </p>
+            <a href="#">Contact us &rarr;</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
